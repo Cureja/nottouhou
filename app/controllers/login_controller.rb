aab147896cc99a5266b464e29859d7f166251a3f
Req <lrkulak@gmail.com>
2017-10-24 13:46:24 -0700

    add server side login/register

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,16 @@
 
 class LoginController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def create
-    render plain: "foo"
+    username = params[:username]
+    password = params[:password]
+    user = User.find_by(username: username)
+    if user.nil? || !user.authenticate(password) then
+      render plain: "0 Invalid username or password"
+      return
+    end
+    # should be good
+    render plain: "1"
   end
 end
--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,6 +1,26 @@
 
 class RegisterController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def create
-    render plain: "bar"
+    username = params[:username]
+    if !username.match(/[a-zA-Z0-9\-]{3,20}/) then
+      render plain: "0 Invalid username"
+      return
+    end
+    password = params[:password]
+    if !username.match(/[a-zA-Z0-9]{6,20}/) then
+      render plain: "0 Invalid password"
+      return
+    end
+    user = User.find_by(username: username)
+    if !user.nil? then
+      # user already exists
+      render plain: "0 Username taken"
+      return
+    end
+    user = User.create(username: username, password: password, password_confirmation: password)
+    # should be good
+    render plain: "1"
   end
 end
