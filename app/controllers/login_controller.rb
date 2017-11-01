e95b3d68770abe00b837de02421632ccfea9a6d8
Req <lrkulak@gmail.com>
2017-11-01 15:04:07 -0700

    use rails idioms instead of homebrew
    csrf protection also re-enabled

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,26 +1,34 @@
 
 class LoginController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  add_flash_types :error, :username, :password
+  
+  def add_error(e)
+    if flash[:error].length > 0 then
+      flash[:error] += "<br>"
+    end
+    flash[:error] += e
+  end
+  
+  def do_error
+    flash[:username] = @username
+    flash[:password] = @password
+    redirect_to :controller => "login", :action => "index"
+  end
   
   def create
-    username = params[:username]
-    password = params[:password]
-    user = User.find_by(username: username)
-    if user.nil? || !user.authenticate(password) then
-      render :json => {
-        :error => "Invalid username or password"
-      }
-      return
+    flash[:error] = ""
+    @username = params[:username]
+    @password = params[:password]
+    user = User.find_by(username: @username)
+    if user.nil? || !user.authenticate(@password) then
+      add_error "Invalid username or password"
     end
-    session[:user_id] ||= user.id
-    
-    redirect_if_logged_in
-    
-    
-    #render :json => {
-    #  :redirect => "../profile/",
-    #  :authtoken => ""
-    #}
+    if flash[:error].length > 0 then
+      do_error
+    else
+      session[:user_id] ||= user.id
+      redirect_if_logged_in
+    end
   end
   
   def index
--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,45 +1,46 @@
 
-class RegisterController < ApplicationController
-  skip_before_action :verify_authenticity_token
+class RegisterController < ApplicationController  
+  add_flash_types :error, :username, :password, :password_confirm
+  
+  def add_error(e)
+    if flash[:error].length > 0 then
+      flash[:error] += "<br>"
+    end
+    flash[:error] += e
+  end
+  
+  def do_error
+    flash[:username] = @username
+    flash[:password] = @password
+    flash[:password_confirm] = @password_confirm
+    redirect_to :controller => "register", :action => "index"
+  end
   
   def create
-    username = params[:username]
-    if !username.match(/[a-zA-Z0-9\-]{3,20}/) then
-      render plain: "0 Invalid username"
-      return
+    flash[:error] = ""
+    @username = params[:username]
+    @password = params[:password]
+    @password_confirm = params[:password_confirm]
+    if !@username.match(/[a-zA-Z0-9\-]{3,20}/) then
+      add_error "Invalid username"
     end
-    password = params[:password]
-    password2 = params[:password_confirm]
-    if password != password2 then
-      render :json => {
-        :error => "Passwords do not match"
-      }
-      return
+    if !@password.match(/[a-zA-Z0-9]{6,20}/) then
+      add_error "Passwords contains illegal characters or has illegal length"
     end
-    if !password.match(/[a-zA-Z0-9]{6,20}/) then
-      render :json => {
-        :error => "Passwords contains illegal characters or has illegal length"
-      }
-      return
+    if @password != @password_confirm then
+      add_error "Passwords do not match"
     end
-    user = User.find_by(username: username)
+    user = User.find_by(username: @username)
     if !user.nil? then
-      render :json => {
-        :error => "Username is taken"
-      }
-      return
+      add_error "Username is taken"
     end
-    user = User.create(username: username, password: password, password_confirmation: password)
-    session[:user_id] ||= user.id
-    
-    # TODO use flash object for error messages
-    
-    redirect_if_logged_in
-    
-    # render :json => {
-    #   :redirect => "../profile/",
-    #   :authtoken => ""
-    # }
+    if flash[:error].length > 0 then
+      do_error
+    else
+      user = User.create(username: @username, password: @password, password_confirmation: @password)
+      session[:user_id] ||= user.id
+      redirect_if_logged_in
+    end
   end
   
   def index
