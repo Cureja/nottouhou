c36fc8b888850043c2fb3abf5d20bddb43c71dc8
Alvin_Ho <hoalvinh@sfu.ca>
2017-11-04 20:22:44 -0700

    fixed User methods and gitignore

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,12 +26,18 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
 
+    def authenticated?(remember_token)
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+  public
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string,cost: cost)
+    end
+
     def remember
       self.remember_token = User.new_token
       update_attribute(:remember_digest, User.digest(remember_token))
-    end
-
-    def authenticated?(remember_token)
-      BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
 end
