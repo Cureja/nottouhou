7d05c0e55de10a1b56ec95239bf66cd95aab757e
Alvin_Ho <hoalvinh@sfu.ca>
2017-11-06 19:21:18 -0800

    added tests

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,6 @@
 
 class GameController < ApplicationController
   def index
-    user = session[:user_id]
-    if user.nil? then
-      redirect_to :controller => "login", :action => "index"
-    end
+    redirect_if_logged_in
   end
 end
