4e8b555c35489578b5406b63bca3e6bf95265add
Andy W <wuandyw@sfu.ca>
2017-11-17 21:00:22 -0800

    made the profile look better.
    further improvements

--- public/stages/stage0.js
+++ public/stages/stage0.js
@@ -1,57 +1,49 @@
-
-/*
- - Every entity creation should have a respective dispatch() call.
- - All events for a level should be added between the master entity creation and dispatch calls.
- - There should (generally, not a rule) be one master.addEvent() call per enemy spawned.
- - Projectiles should (again, generally) be spawned by an enemy, but their parent set to master.
-   Not doing the former will cause them to spawn even after the entity that spawns them dies (among other things).
-   Not doing the latter will cause them to be (improperly!) garbage collected when their spawner dies.
-*/
-
 let MASTER = new Entity(null, "MASTER", 0, 0);
-
-/*
-spawn enemy at 5 seconds in at the top middle of the screen
-2 seconds later, enemy shoots 2 projectiles straight down, spawning on either side of the enemy
-1 seconds later, enemy moves to center of screen (over 2 seconds)
-*/
 
 MASTER.addEvent(1000, function(MASTER) {
 	new Enemy(MASTER, "playerIdle", app.renderer.width / 2, 48, 5).addEvent(1000, function(enemy) {
-		let projectiles = [
-			new BoundedProjectile(MASTER, "projectileKnifeIdle180", enemy.handle.x - 10, 8, 1),
-			new BoundedProjectile(MASTER, "projectileKnifeIdle180", enemy.handle.x + 10, 8, 1)
-		];
-		for (var k = 0; k < 2; k++) {
-			projectiles[k].addEvent(0, function(self) {
-				self.handle.y += 2;
-				return 80;
-			});
+		let coneProjectiles =  [];
+		//Fires 30 projectiles
+		for (var i = 0; i < 3; i++) {
+			coneProjectiles.push(new BoundedProjectile(MASTER, "projectileKnifeIdle45", enemy.handle.x - 10, 8, 1))
+      		coneProjectiles.push(new BoundedProjectile(MASTER, "projectileKnifeIdle135", enemy.handle.x + 10, 8, 1))
+      		coneProjectiles.push(new BoundedProjectile(MASTER, "projectileKnifeIdle180", enemy.handle.x, 8, 1))
 		}
-		for (var k = 0; k < 2; k++) {
-			projectiles[k].dispatch(enemyProjectiles);
+		var counter = 0;
+		for (var i = 0; i < 3; i++) {
+			for (var j = 0; j < 3; j++) {
+				//This is for the 45 degrees so curve left
+				if (j == 0) {
+					coneProjectiles[counter].addEvent(100, function(self) {
+					self.handle.y += 2;
+					self.handle.x -= 0.5;
+					return 80;
+					});
+		    	}
+		      	//This is for the 135 degree so curve right
+		      	else if (j == 1) {
+					coneProjectiles[counter].addEvent(100, function(self) {
+					self.handle.y += 2;
+					self.handle.x += 0.5;
+					return 80;
+		        	});
+		    	}
+		      	//This shoots straight down
+				else if (j == 2) {
+					coneProjectiles[counter].addEvent(100, function(self) {
+					self.handle.y += 2;
+					return 80;
+					});
+				}
+				counter = counter + 1;
+			}
 		}
-		return REMOVE_EVENT;
-	}).dispatch();
-});
+    for (var k = 0; k < 9; k++) {
+    	  coneProjectiles[k].dispatch(enemyProjectiles);
+    }
 
-MASTER.addEvent(2000, function(MASTER) {
-	new Enemy(MASTER, "playerIdle", app.renderer.width / 3, 48, 5).addEvent(1000, function(enemy) {
-		let projectiles = [
-			new BoundedProjectile(MASTER, "projectileKnifeIdle180", enemy.handle.x - 10, 8, 1),
-			new BoundedProjectile(MASTER, "projectileKnifeIdle180", enemy.handle.x + 10, 8, 1)
-		];
-		for (var k = 0; k < 2; k++) {
-			projectiles[k].addEvent(0, function(self) {
-				self.handle.y += 2;
-				return 80;
-			});
-		}
-		for (var k = 0; k < 2; k++) {
-			projectiles[k].dispatch(enemyProjectiles);
-		}
-		return REMOVE_EVENT;
 	}).dispatch();
+	return 10000;
 });
 
 MASTER.dispatch();
