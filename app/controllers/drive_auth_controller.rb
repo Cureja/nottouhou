a1e93a28e4d860b25c272744c7f09def0982e353
Req <lrkulak@gmail.com>
2017-11-20 18:40:44 -0800

    for posterity

--- app/controllers/drive_auth_controller.rb
+++ app/controllers/drive_auth_controller.rb
@@ -1,6 +1,5 @@
 
-require "google/apis/drive_v2"
-require "google/api_client/client_secrets"
+require "googleauth"
 
 class DriveAuthController < ApplicationController
   def index
@@ -10,18 +9,25 @@ class DriveAuthController < ApplicationController
   def create
     redirect_if_not_logged_in
     user = current_user
-    auth_client = $client_secrets.to_authorization
-    auth_client.update!(
-      :scope => "https://www.googleapis.com/auth/drive.file",
-      :redirect_uri => "postmessage",
+    credentials = Google::Auth::UserRefreshCredentials.new(
+      client_id: "520788856982-o79jg4d27s9ogvs358unqnntiv7k52ah.apps.googleusercontent.com",
+      client_secret: "L5lNtMEmS6tGXXpMAdFkxrRd",
+      scope: ["https://www.googleapis.com/auth/drive.file"],
+      redirect_uri: "postmessage"
     )
-    auth_client.code = params[:authcode]
-    auth_client.fetch_access_token!
-    auth_client.client_secret = nil
-    user.drive_cred = auth_client.to_json
-    user.save
+    credentials.code = params[:authcode]
+    credentials.fetch_access_token!
     p "successfully got drive cred for user #{user.id}"
-    DriveState.init_for_session(session)
+    credentials.client_secret = nil
+    user.drive_cred = credentials.to_json
+    user.save
+    p "putting a file"
+    replay = Replay.new
+    replay.init_service(user)
+    #replay.service = GoogleDrive::Session.from_credentials(credentials)
+    replay.events = [1,2,3]
+    replay.store
+    p "success"
     render :json => {
       :success => true
     }
--- app/models/replay.rb
+++ app/models/replay.rb
@@ -1,5 +1,5 @@
 
-require "google/api_client"
+require "google_drive"
 
 class Replay < ApplicationRecord
   "
@@ -11,17 +11,43 @@ class Replay < ApplicationRecord
     then call `Replay.retrieve`. Now you can access `replay.events`.
   "
 
+  attr_accessor :events
+  attr_accessor :service
+
   def initialize
-    @events = [];
+    @events = []
+    @service = nil
+  end
+
+  def init_service(authcode, user)
+    @user = user
+    credentials = Google::Auth::UserRefreshCredentials.new(
+      client_id: "520788856982-o79jg4d27s9ogvs358unqnntiv7k52ah.apps.googleusercontent.com",
+      client_secret: "L5lNtMEmS6tGXXpMAdFkxrRd",
+      scope: ["https://www.googleapis.com/auth/drive.file"],
+      redirect_uri: "postmessage"
+    )
+    credentials.code = authcode
+    credentials.fetch_access_token!
+    
+  end
+
+
+
+  def init_service(user)
+    #@service ||= GoogleDrive::Session.from_access_token(user.drive_cred)
+    @service = GoogleDrive::Session.login_with_oauth(user.drive_cred)
   end
 
   def store
-    file = drive.
+    serialized = Marshal.dump(@events)
+    file = @service.upload_from_string(serialized)
+    self.replay_id = file.id
   end
 
   def retrieve
-
-    file = service.get(:fileId => self.replay_id)
+    # Marshal.load
+    # @service.file_by_id
   end
 
   def delete
