35d8ccf8b2c8beb721401f75fb99dc026259c718
Req <lrkulak@gmail.com>
2017-11-21 22:53:15 -0800

    update postreplay controller and add instructions for using replays

--- app/controllers/post_replay_controller.rb
+++ app/controllers/post_replay_controller.rb
@@ -1,19 +1,20 @@
 
-class PostReplayController < ApplicationController
-  # post to here with { events: JSON.stringify(events) }
+require "drive_state"
+require "replay_wrap"
 
+# post to here with { events: JSON.stringify(events) }
+
+class PostReplayController < ApplicationController
   def create
     user = current_user
     if user.nil? then
       return
     end
 
-    p params[:events]
-
-    drivestate = Replay::DriveState.new(user)
+    drivestate = DriveState.new(user)
     drivestate.recreate
 
-    replay = Replay.new
+    replay = ReplayWrap.new(user: user)
     replay.service = drivestate.service
     replay.events = JSON.parse(params[:events])
     replay.store
--- app/models/replay.rb
+++ app/models/replay.rb
@@ -1,11 +1,18 @@
 
 class Replay < ApplicationRecord
   "
+  To store/retrieve replays, you need a DriveState.
+    drivestate = DriveState.new(user)
+    # assuming the user has already linked drive
+    drivestate.recreate
+
   Make a replay with:
     replay = ReplayWrap.new
   See replay_wrap.rb for specifics on arguments to the constructor.
   Give it events:
     replay.events = arrayofevents
+  Give it a DriveState service so it can connect to drive:
+    replay.service = drivestate.service
   Store it in drive:
     replay.store
   Maybe you want to find replays by user:
