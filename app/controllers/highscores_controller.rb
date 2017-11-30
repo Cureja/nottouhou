451d1c652da626d0e828ac753a52d8ba13a37656
mgergely <mgergely@asb9840u-e07.csil.sfu.ca>
2017-11-30 04:57:59 -0800

    fixed highscores ordering, added pages

--- app/controllers/highscores_controller.rb
+++ app/controllers/highscores_controller.rb
@@ -1,10 +1,13 @@
 class HighscoresController< ApplicationController
   skip_before_action :verify_authenticity_token
   helper_method :put_entry
+  helper_method :get_page
 
   def index
     @user = current_user
     @count = Highscore.count
+    @pages = (@count/20.to_f).ceil
+    @intpage = 1
     if @user.nil? then
       redirect_to :controller => "login", :action => "index"
     end
@@ -17,10 +20,15 @@ class HighscoresController< ApplicationController
   end
 
   def put_entry(value)
-    highscore = Highscore.offset(value).first
+    highscore = Highscore.order('score DESC, created_at').offset(value).first
     if !highscore.nil?
-      return highscore.username + " | Kills: #{highscore.score}"
+      return highscore.username + " |  score: #{highscore.score}"
     end
+  end
+
+  def get_page
+    @page = CGI.parse((URI.parse(request.original_url)).query)['page'].first
+    @intpage = @page.to_i
   end
 
 end
