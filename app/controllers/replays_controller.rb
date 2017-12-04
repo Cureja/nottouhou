36db35ef00b7ccae2d8f4304d987ca12fe9f7ab4
hoalvinh <hoalvinh@cs-sc4080u-a02.cs.surrey.sfu.ca>
2017-12-04 11:38:14 -0800

    Fixed style

--- app/controllers/replays_controller.rb
+++ app/controllers/replays_controller.rb
@@ -1,7 +1,13 @@
 class ReplaysController < ApplicationController
   skip_before_action :verify_authenticity_token
   helper_method :get_replay
+  helper_method :get_stage
+  helper_method :get_time
+  helper_method :get_page
   def index
+    @count = Replay.count
+    @pages = (@count/20.to_f).ceil
+    @intpages = @pages.to_i
     @user = current_user
     @drivestate = DriveState.new(@user)
     # if drivestate -- Check if has drivestate
@@ -11,11 +17,11 @@ class ReplaysController < ApplicationController
     end
   end
 
-  def get_replay
+  def get_replay(value=1)
     if not @user.drive_refresh_token.nil? 
     	replay = ReplayWrap.new(user: @user)
       replay.service = @drivestate.service
-      id = Replay.find_by(:user_id => @user.id).replay_id
+      id = Replay.order('created_at').offset(value).first.replay_id
       if !id.nil? 
         replay.replay.replay_id = id
         replay = replay.retrieve
@@ -23,4 +29,23 @@ class ReplaysController < ApplicationController
     end
   end
   
+  def get_time(value=1)
+    time = Replay.order('created_at').offset(value).first
+    if !time.nil?
+      return time.created_at
+    end
+   end
+
+  def get_stage(value=1)
+    stage = Replay.order('created_at').offset(value).first
+    if !stage.nil?
+      return stage.stage
+    end
+  end
+
+  def get_page
+    @page = CGI.parse((URI.parse(request.original_url)).query)['page'].first
+    @intpage = @page.to_i
+  end
+
 end
